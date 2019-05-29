36215af58a525768a4aacb0d3b47fcf5b0b66eaa
Connor Minielly <Connor_Minielly@Outlook.com>
2019-05-28 23:00:39 -0400

    feature(shuriken): updated shuriken component, shifted animation action into component, made a e s t h e t i c.

--- src/shared/components/Shuriken.js
+++ src/shared/components/Shuriken.js
@@ -1,10 +1,72 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import shurikenImg from 'shared/images/Shuriken.svg';
+import PropTypes from 'prop-types';
+import {
+  TweenLite, TimelineLite, Expo, Sine,
+} from 'gsap';
+import styled from 'styled-components';
 
-const Shuriken = () => (
-  <div>
-    <img src={shurikenImg} alt="ninja style" />
-  </div>
-);
+const ShurikenBox = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  background-image: url(${shurikenImg});
+  background-size: cover;
+  height: 200px;
+  width: 200px;
+`;
+
+const Shuriken = ({ position }) => {
+  let boxRef = useRef(null);
+
+  // init the animation timeline.
+  const timeline = new TimelineLite();
+
+  // Run once
+  useEffect(() => {
+    TweenLite.set(boxRef, { xPercent: -50, yPercent: -50 });
+  }, []);
+
+  // Run whenever position changes
+  useEffect(() => {
+    timeline
+      .fromTo(
+        boxRef,
+        1,
+        { rotation: 0, transformOrigin: '50% 50%' },
+        {
+          rotation: 720,
+          repeatDelay: 0,
+          ease: Expo.easeOut,
+        },
+        0,
+      )
+      .to(
+        boxRef,
+        0.5,
+        {
+          x: position.x,
+          y: position.y,
+          ease: Sine.easeOut,
+        },
+        0,
+      );
+  }, [position]);
+
+  return (
+    <ShurikenBox
+      ref={(element) => {
+        boxRef = element;
+      }}
+    />
+  );
+};
+
+Shuriken.propTypes = {
+  position: PropTypes.shape({
+    x: PropTypes.number,
+    y: PropTypes.number,
+  }).isRequired,
+};
 
 export default Shuriken;
