8e3acf861f4add2a6f361fe75ff2666c6634f7d0
Connor Minielly <Connor_Minielly@Outlook.com>
2019-05-28 23:00:39 -0400

    feature(shuriken): updated shuriken component, shifted animation action into component, made a e s t h e t i c.

--- src/App/App.js
+++ src/App/App.js
@@ -1,45 +1,31 @@
-import { TweenMax, Linear } from 'gsap';
-import React, { useEffect, useRef } from 'react';
-
+import React, { useEffect, useState } from 'react';
+import Title from 'shared/components/Title';
 import Shuriken from 'shared/components/Shuriken';
-import Box from 'shared/components/Box';
 import Layout from 'shared/components/Layout';
 
 import GlobalStyle from 'shared/styles/GlobalStyle';
 
 const App = () => {
-  let boxRef = useRef(null);
+  const [position, setPosition] = useState({
+    x: window.innerWidth / 2,
+    y: window.innerHeight / 4,
+  });
 
   useEffect(() => {
-    TweenMax.set(boxRef, { xPercent: -50, yPercent: -50 });
-
-    // Start default spinning animation for the box.
-    TweenMax.fromTo(
-      boxRef,
-      2,
-      { rotation: 0 },
-      {
-        rotation: 360,
-        repeat: -1,
-        repeatDelay: 0,
-        ease: Linear.easeNone,
-      },
-    );
-
     document.addEventListener('click', (event) => {
       const { clientX, clientY } = event;
-      TweenMax.to(boxRef, 1, { x: clientX, y: clientY });
+      setPosition({
+        x: clientX,
+        y: clientY,
+      });
     });
   }, []);
 
   return (
     <>
       <Layout>
-        <Shuriken
-          ref={(element) => {
-            boxRef = element;
-          }}
-        />
+        <Title>s u n s e t s h u r i k e n</Title>
+        <Shuriken position={position} />
       </Layout>
       <GlobalStyle />
     </>
---
+++ src/shared/components/Title.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  pointer-events: none;
+  user-select: none;
+  font-family: serif;
+  background-color: palevioletred;
+  color: palegoldenrod;
+  box-shadow: 7px 10px #7cb4cd;
+`;
+
+export default Title;
