376d453137c87eac57c8c095989f6485aa01850a
dyyyl <hey@dyyyl.rocks>
2019-06-22 14:07:40 +0200

    slower animation, less distracting

--- src/shared/components/Star.js
+++ src/shared/components/Star.js
@@ -4,7 +4,7 @@ const Star = styled.span`
   display: inline-block;
   color: black;
   margin: auto 1rem;
-  animation: spin infinite 5s linear;
+  animation: spin infinite 7s linear;
   @keyframes spin {
     from {
       transform: rotate(0deg);
