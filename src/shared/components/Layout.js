88d377deb65bd572ceae0caa1ad5c776806d6b6d
dyyyl <hey@dyyyl.rocks>
2019-06-22 14:08:09 +0200

    add quandrants

--- src/shared/components/Layout.js
+++ src/shared/components/Layout.js
@@ -2,10 +2,14 @@ import styled from 'styled-components';
 
 const Layout = styled.main`
   height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  display: grid;
   background-color: palegoldenrod;
+
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 1fr 1fr;
+  grid-template-areas:
+    "top-left top-right"
+    "bottom-left bottom-right";
 `;
 
 export default Layout;
